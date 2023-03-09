36603ebc56c9a75fd56d7ee50c3be13518e93632
maduraikaran <127435668+maduraikaran@users.noreply.github.com>
2023-03-09 23:34:58 +0530

    Add files via upload

---
+++ next3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title> 
+       <link rel="stylesheet" href="../css/styles3.css">
+
+</head>
+<body>
+<div class="login">
+      <div class="light"> <a href="on.php"><button>Turn On</button></a></div>
+      <div class="Fan"><a href="off.php"><button>Turn Off</button></a></div>
+</div>
+    
+</a>
+</body>
+</html>
---
+++ onf.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'conn.php';
+
+$update_query = "UPDATE devices_status SET device_status='1' WHERE id='2';";
+$conn->query($update_query);
+
+header("Location: next3.php");
+
+echo "<script> console.log('Light Turned On ');</script>";
+
+
+
+?> 
